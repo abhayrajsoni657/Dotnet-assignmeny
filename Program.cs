c10849fc9a72d59d72d80aad79d7e28971818da5
abhayrajsoni657 <63549431+abhayrajsoni657@users.noreply.github.com>
2022-12-19 13:39:46 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppLogical
+{
+    class TrianglePattern
+    {
+        static void main(String[] args)
+        {
+            int spc, len = 1, count;
+            Console.WriteLine("Enter the row : ");
+            count = Convert.ToInt32(Console.ReadLine());
+            spc = count - 1;
+            for (int i = 1; i <= spc; i++)
+            {
+                for (int r = 1; r <= spc; r++)
+                {
+                    Console.WriteLine(" ");
+                }
+                for (int s = 1; s <= len; s++)
+                {
+                    Console.WriteLine("*");
+                }
+                spc--;
+                len = len + 2;
+                Console.WriteLine();
+            }
+            Console.Readline();
+        }
+    }
+}
